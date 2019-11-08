9b307be4e8132f40770d67ca66b04d5fdb4dfc38
Jean-luc <github-commit-email@jean-luc.org>
2019-11-07 18:12:55 -0800

    add indexes

---
+++ rir/migrations/2019-11-08-020618_indexes/down.sql
@@ -0,0 +1,4 @@
+-- This file should undo anything in `up.sql`
+DROP INDEX standings_sets_a;
+DROP INDEX standings_a;
+DROP INDEX pages_a;
---
+++ rir/migrations/2019-11-08-020618_indexes/up.sql
@@ -0,0 +1,4 @@
+-- Your SQL goes here
+CREATE INDEX standings_sets_a ON standings_sets (ty, finished_at);
+CREATE INDEX standings_a ON standings (set_rowid, is_anon, accrued_time);
+CREATE INDEX pages_a ON pages (page_num,valid,valid_html);
