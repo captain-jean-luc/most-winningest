6aafcbd64ad6b1adf7f82f5550b91e21b3eea0a3
Shelvacu <git@shelvacu.com>
2025-02-14 18:43:56 -0800

    stuff

--- package.nix
+++ package.nix
@@ -6,6 +6,7 @@
 }: rustPlatform.buildRustPackage {
   pname = "most-winningest";
   version = "69.420";
+  auditable = false; #cargo-auditable in nixpkgs doesn't support 2024 edition yet
 
   nativeBuildInputs = [ pkg-config ];
 
